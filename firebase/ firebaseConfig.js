0c19bcf23cc886992789ee86dacbcc8b9fdefb67
govind-mit <govind@mangoitsolutions.com>
2022-08-30 18:25:06 +0530

    added header

--- firebase/ firebaseConfig.js
+++ firebase/ firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLg6eXtrE8NZczijknyPnLGxJ4YkOWk3c",
-  authDomain: "wrybrit-databases.firebaseapp.com",
-  projectId: "wrybrit-databases",
-  storageBucket: "wrybrit-databases.appspot.com",
-  messagingSenderId: "915717983087",
-  appId: "1:915717983087:web:56501c60b5a1efcbb4769a",
-  measurementId: "G-KN4D8L0102",
+  apiKey: "AIzaSyB3hLtCicT2IZfkbI7BjDQXKjJmLGV_elQ",
+  authDomain: "wyrbritshop.firebaseapp.com",
+  databaseURL: "https://wyrbritshop-default-rtdb.firebaseio.com",
+  projectId: "wyrbritshop",
+  storageBucket: "wyrbritshop.appspot.com",
+  messagingSenderId: "573257655209",
+  appId: "1:573257655209:web:133dffa370a06cd9bedab8",
+  measurementId: "G-M63N0VEX7S",
 };
 
 export const app = initializeApp(firebaseConfig);
